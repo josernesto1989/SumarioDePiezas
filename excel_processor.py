01e70e7d7091e9c907e814aea2295efb745b53bf
josernesto1989 <josernesto1989@gmail.com>
2025-09-17 11:39:28 -0600

    fix: closing on read data step

--- excel_processor.py
+++ excel_processor.py
@@ -11,15 +11,18 @@ class ExcelProcessor:
     def process_file(self, file_path_list, output_directory):
         tipos_de_piezas =["ASS","LCD", "LCD GALAXY TAB", "LCD NO ANDROID", 'LCD TAB 7"', 'LCD TAB 9"', 'LCD TAB 10"', 'TOUCH AND', 'TOUCH GTAB', 'TOUCH IPAD', 'TOUCH NO ANDROID', 'TOUCH TAB 8"', 'TOUCH TAB 7.85"', 'TOUCH TAB 7"', 'TOUCH TAB 9"', 'TOUCH TAB 10"', 'ANILLOS PERS', 'BAT DET', 'BAT', 'BAT INTERNA', 'CD', 'CARGADOR PF', 'CARGADOR UNIV', 'COV PERS', 'COV', 'COV DET', 'GEVEY', 'ML BT', 'ML CABLE', 'MEM USB 16GB', 'MCP AAA', 'MCP FIBRA CARBON', 'MCP LARGE', 'MCP MEDIUM', 'MCP R', 'MCP XLARGE', 'MCP XSMALL', 'MC', 'MSD 8GB', 'MSD 16GB', 'MSD 32GB', 'MSD 64GB', 'OTROS', 'PTA 1A', 'PTA 2A', 'RELOJ', 'TARJETA INT', 'TELEF', 'CAMARAS', 'BOCINAS', 'BOTONES, JOY, BSIM', 'CRISTALES', 'FLEX POWER, VOL, ETC', 'FLEX', 'PC CONECTOR 1', 'PC CONECTOR 2', 'PC CONECTOR 3', 'PC CONECTOR 4', 'PSIM', 'TAPAS TRASERAS', 'PC FLEX', 'PLACAS', 'BAT ADAP', 'ANILLOS UNIV', 'TOUCH REC', 'LCD REC', 'ASS SIN CRISTAL', 'MC SIN CLASIF', 'CD MAGNETICO', 'MSD 128GB', 'MSD 256GB', 'MEM USB 32GB', 'MEM USB 64GB', 'MEM USB 128GB', 'COV SIN CLASIF', 'MICROFONOS', 'COV LENTO MOV', 'CRISTALES CAM', 'BAT COMO ADAP', 'BAT DET COMO ADAP', 'MHG']
         total_result={}
-        archivos = [x.split('/')[:-5] for x in file_path_list]
+        archivos = [x for x in file_path_list]
+        print(file_path_list)
+        print(f'Archivos a procesar: {archivos}')
         for archivo in archivos:
             try:
+                print(f'Abriendo {archivo}')
                 # Cargar el archivo Excel
                 workbook = openpyxl.load_workbook(archivo)
                 print(f'Revisando{archivo}')
                 # Obtener las primeras 7 hojas
                 hojas = workbook.worksheets[:7]
-                fileName = archivo
+                fileName = archivo.split('/')[-1][:-5]
                 print(f'llegando ')
                 result={}
                 # Iterar sobre cada hoja y realizar alguna acción 
